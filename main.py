151e067acab30992962fc4f5a275f0014090abe6
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2020-12-21 12:18:25 -0600

    conditional stuff

--- main.py
+++ main.py
@@ -15,18 +15,23 @@ files = os.listdir(path)
 for root, directories, files in os.walk(path, topdown=False):
     for name in files:
 
-        #   Load mp3 file
+        #   Load MP3 file
         if not name == '.DS_Store':
             audio_file = eyed3.load(os.path.join(root, name))
 
             #   Variables
             title = audio_file.tag.title
             artist = audio_file.tag.artist
+            #   If the artist ID3 name contains a /, replace with &
+            if artist.find("/") != -1:
+                artist = artist.replace("/", "&")
             album = audio_file.tag.album
 
             if not os.path.isdir(os.path.join(root, artist)):
                 os.makedirs(os.path.join(root, artist))
                 print(f"Directory '{artist}' didn't exist, making it now.")
+
+            #   Move mp3 to
 
             #   Print each file path
             print(f"-------------------------------\n"
@@ -34,7 +39,8 @@ for root, directories, files in os.walk(path, topdown=False):
                   f"Corrected Path: {os.path.join(root, artist, album, title)}.mp3")
 
         #   TODO
+        #   - [DONE] Conditional replace for / in 'artist' field with '&'
+        #   - [DONE] Conditional function to check if directory exists
         #   - Conditional function for duplicates
-        #   - Conditional function to check if directory exists
         #   - Conditional function to check if 'artist' field is null
         #   - Conditional function to check if 'contributing artist' field is null
