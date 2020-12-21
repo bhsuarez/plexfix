4202b97f6754abc6d7f9f7b5f59c861145260a78
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2020-12-20 21:35:19 -0600

    yep

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import eyed3
 import logging
 import os
+
+#   Stupid error
 logging.getLogger("eyed3.mp3.headers").setLevel(logging.CRITICAL)
 
+#   Set path
 path = 'media'
 
+#   List directory
 files = os.listdir(path)
 
+#   Loop through directory
 for root, directories, files in os.walk(path, topdown=False):
     for name in files:
+
+        #   Load mp3 file
         audio_file = eyed3.load(os.path.join(root, name))
 
-        # Variables
+        #   Variables
         title = audio_file.tag.title
         artist = audio_file.tag.artist
         album = audio_file.tag.album
 
-        # print(f"{audio_file.tag.artist} / {audio_file.tag.album} / {audio_file.tag.title}")
+        #   Print each file path
         print(f"Full Path: {os.path.join(root, artist, album, title)}.mp3")
