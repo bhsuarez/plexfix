ed6d1cca4fd277e677d7e5f9f7d0d92d842d2ccd
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2020-12-21 16:05:48 -0600

    null artist check

--- main.py
+++ main.py
@@ -20,32 +20,41 @@ for root, directories, files in os.walk(path, topdown=False):
         if not name == '.DS_Store':
             audio_file = eyed3.load(os.path.join(root, name))
 
-            #   Variables
+            #   Set Variables
             title = audio_file.tag.title
             artist = audio_file.tag.artist
             #   If the artist ID3 name contains a /, replace with &
             if artist.find("/") != -1:
                 artist = artist.replace("/", "&")
+            if artist == ' ' or artist == '':
+                artist = "Unknown Artist"
             album = audio_file.tag.album
 
             #   Files
             original_path = os.path.join(root, name)
-            destination_path = os.path.join(root, artist, album, title)+".mp3"
+            destination_path = os.path.join(root, artist, album, title) + ".mp3"
+
+            #   Get Size
+            size = str(os.path.getsize(original_path))
 
             #   Create artist directory if it doesn't exist
             if not os.path.isdir(os.path.join(root, artist)):
-                os.makedirs(os.path.join(root, artist))
+
+                #os.makedirs(os.path.join(root, artist))
                 print(f"Directory '{artist}' didn't exist, making it now.")
 
             #   Create album directory if it doesn't exist
             if not os.path.isdir(os.path.join(root, artist, album)):
-                os.makedirs(os.path.join(root, artist, album))
+
+                #os.makedirs(os.path.join(root, artist, album))
                 print(f"Directory '{artist} / {album}' didn't exist, making it now.")
 
             #   Move file
-            shutil.move(original_path, destination_path)
+            #shutil.move(original_path, destination_path)
 
             #   Print each file path
             print(f"-------------------------------\n"
                   f"Original Path: {original_path}\n"
-                  f"Corrected Path: {destination_path}")
+                  f"Corrected Path: {destination_path}\n"
+                  f"Size: {size}\n"
+                  f"-------------------------------\n")
