a61143ef8d5e661d85c6f3e8bad6b1f4680b6623
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2020-12-31 10:31:55 -0600

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,7 @@
 #
-#   1.  Remove media folder
-#   2.  Duplicate media_backup folder
-#   3.  Rename media_backup folder to media
+#   1.  Remove /media folder
+#   2.  Unzip media.zip to /media
 #
+
+import shutil
+
